43369022943686bc58d6772ee82769f0f0bb159b
Jean-Baptiste Le Duigou <jean-baptiste.leduigou@aircall.io>
2022-11-15 20:49:27 +0100

    ✅ write all unit tests for service

---
+++ lambda-stats/mock.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/request"
+	"github.com/aws/aws-sdk-go/service/lambda"
+	"github.com/aws/aws-sdk-go/service/lambda/lambdaiface"
+	"github.com/stretchr/testify/mock"
+)
+
+type mockedLambda struct {
+	lambdaiface.LambdaAPI
+	mock.Mock
+}
+
+func (m *mockedLambda) ListFunctionsWithContext(ctx aws.Context, input *lambda.ListFunctionsInput, opts ...request.Option) (*lambda.ListFunctionsOutput, error) {
+	ret := m.Called(ctx, input, opts)
+	if ret.Get(1) != nil {
+		return nil, ret.Get(1).(error)
+	}
+	return ret.Get(0).(*lambda.ListFunctionsOutput), nil
+}
--- lambda-stats/service_test.go
+++ lambda-stats/service_test.go
@@ -2,20 +2,63 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"testing"
 
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/stretchr/testify/mock"
+
+	"github.com/aws/aws-sdk-go/service/lambda"
 	"github.com/stretchr/testify/assert"
 )
 
-func TestGetLambdaFunctionsShouldReturnPouet(t *testing.T) {
-	s, err := NewLambdaService("us-east-1")
+func TestNewLambdaServiceShouldConfigureRegion(t *testing.T) {
+	svc, err := NewLambdaService("us-east-1")
 
 	assert.Nil(t, err)
 
-	stats, err := s.GetLambdaFunctions(context.Background())
+	assert.Equal(t, "us-east-1", *(svc.(*lambdaServiceImpl).l.(*lambda.Lambda).Client.Config.Region))
+}
 
-	assert.Nil(t, err)
+func TestGetLambdaFunctions(t *testing.T) {
+	ctx := context.Background()
 
-	assert.Len(t, stats, 1)
-	assert.Equal(t, "pouet", stats[0])
+	for _, tc := range []struct {
+		name          string
+		setupMock     func(m *mockedLambda)
+		wantFunctions []string
+		wantErr       string
+	}{
+		{
+			name: "should return error",
+			setupMock: func(m *mockedLambda) {
+				m.On("ListFunctionsWithContext", ctx, mock.Anything, mock.Anything).Return(nil, fmt.Errorf("fake error"))
+			},
+			wantFunctions: []string{},
+			wantErr:       "fake error",
+		},
+		{
+			name: "should return two functions",
+			setupMock: func(m *mockedLambda) {
+				m.On("ListFunctionsWithContext", ctx, mock.Anything, mock.Anything).
+					Return(&lambda.ListFunctionsOutput{Functions: []*lambda.FunctionConfiguration{
+						{FunctionArn: aws.String("arn:aws:lambda:us-east-1:123456789012:function:sam-app-HelloWorldFunction")},
+						{FunctionArn: aws.String("arn:aws:lambda:us-east-1:123456789012:function:sam-app-AlexaFunction")},
+					}},
+						nil)
+			},
+			wantFunctions: []string{"arn:aws:lambda:us-east-1:123456789012:function:sam-app-HelloWorldFunction", "arn:aws:lambda:us-east-1:123456789012:function:sam-app-AlexaFunction"},
+		},
+	} {
+		m := &mockedLambda{}
+		svc := &lambdaServiceImpl{l: m}
+		tc.setupMock(m)
+
+		f, err := svc.GetLambdaFunctions(ctx)
+
+		if err != nil {
+			assert.Equal(t, tc.wantErr, err.Error(), tc.name)
+		}
+		assert.Equal(t, tc.wantFunctions, f, tc.name)
+	}
 }
